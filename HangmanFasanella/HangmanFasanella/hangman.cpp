35c357e875373df3c828781da7b5e5cbf963e102
kmf7094 <kmf7094@IGM2435-18>
2018-02-05 12:10:08 -0500

    Hangman V.2

--- HangmanFasanella/HangmanFasanella/hangman.cpp
+++ HangmanFasanella/HangmanFasanella/hangman.cpp
@@ -2,12 +2,37 @@
 #include "stdafx.h"
 
 void showGallows(int a) {
+	switch(a) {
+		case 6: cout << "_________\n|  |\n|\n|\n|\n|\n|\n";
+			return;
+		case 5:cout << "_________\n|  |\n|  O\n|\n|\n|\n|\n";
+			return;
+		case 4:cout << "_________\n|  |\n|  O\n|  |\n|\n|\n|\n";
+			return;
+		case 3:cout << "_________\n|  |\n|  O\n| /|\n|\n|\n|\n";
+			return;
+		case 2:cout << "_________\n|  |\n|  O\n| /|\\ \n|\n|\n|\n";
+			return;
+		case 1:cout << "_________\n|  |\n|  O\n| /|\\ \n| /\n|\n|\n";
+			return;
+		case 0:cout << "_________\n|  |\n|  O\n| /|\\ \n| / \\ \n|\n|\n";
+			return;
+		default:
+			cout << "_________\n|\n|\n|\n|\n|\n|\n";;
+			return;
+	}
 	
-	cout << "_________\n|\n|\n|\n|\n|\n|\n";
-
 }
 
 void showSolved(char b[], char c[])
 {
+	for (int a = 0; a < strlen(b); a++) {
+		for (int d = 0; d < strlen(c); d++) {
+			if (b[a] == c[d]) {
 
+			}
+		}
+		
+	}
+	cout << "\nWord to solve: ";
 }
