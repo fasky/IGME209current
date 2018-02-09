1af9c738d8ae44fd769e5493c2f98f72c72bcabd
kmf7094 <kmf7094@IGM2435-18>
2018-02-09 12:01:50 -0500

    PE7

--- PointerPEStarterFiles/ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
+++ PointerPEStarterFiles/ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -42,19 +42,37 @@ public:
 
 int main()
 {
-	Monster *boss = new Monster();
-	Player *pOne = new Player();
-	//MovableObject *boss = new Monster();
-	//MovableObject *pOne = new Player();
+	//Monster *boss = new Monster();
+	//Player *pOne = new Player();
+	MovableObject *boss = new Monster();
+	MovableObject *pOne = new Player();
 	Monster *fakeMonster = (Monster*)pOne;
 
 	// add code here
+	cout << "Adresses\nBoss: " << boss << "\nPlayer: " << pOne << "\nFake Monster:" << fakeMonster << endl;
 
+	//set values
+	boss->xPos = 100;
+	boss->yPos = 200;
+	//boss->damage = 30; //if using MovableObject, cant set damage or name since they don't exist in MovableObject
+	//pOne->name = "Catman"; Can't set name of player, MovableObject doesn't have variable 
+	pOne->yPos = 300;
+	fakeMonster->damage = 10; 	//but you can set the fakeMonster's damage. (Monster*)pOne makes it so fakemonster points to pOne values and also has damage variable, but pOne does not have a damage variable
+	fakeMonster->xPos = 400;
 
+	// print pos
+	cout << "Positions\nBoss: ";
+	boss->PrintPos();
+	cout << "Player: ";
+	pOne->PrintPos();
+	cout << "Fake Monster: ";
+	fakeMonster->PrintPos();
+	cout<< "Fake Monster Damage: " << fakeMonster->damage << endl;
 
 	delete boss;
-	delete pOne;
-	delete fakeMonster;
+	delete pOne; //delete the pointer
+	fakeMonster = nullptr; //dangling pointer? set to nullptr since what pOne points to is deleted
+
 
 	_getch();
     return 0;
