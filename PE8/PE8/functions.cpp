266fcdc7aba87d784e9003120ce096df930a70a3
kmf7094 <kmf7094@IGM2435-18>
2018-02-14 11:45:56 -0500

    PE8 v.1

---
+++ PE8/PE8/functions.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+void changeVariable(int)
+{
+}
+
+void changePointer(int *)
+{
+}
+
+int getLengthArray(int[])
+{
+	return 0;
+}
+
+int getLengthPointer(int *)
+{
+	return 0;
+}
+
+int createStackArray()
+{
+	return 0;
+}
+
+int createHeapArray(int)
+{
+	return 0;
+}
---
+++ PE8/PE8/functions.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void changeVariable(int);
+
+void changePointer(int*);
+
+int getLengthArray(int[]);
+
+int getLengthPointer(int*);
+
+int createStackArray();
+
+int createHeapArray(int);
