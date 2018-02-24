cdbf32c28acfc7e9bc1d7868eb3f90fe317b8f6a
Kyle Fasanella <fasanellaky@gmail.com>
2018-02-24 18:20:15 -0500

    Getting there

--- GravitySnake/GravitySnake/snake.cpp
+++ GravitySnake/GravitySnake/snake.cpp
@@ -1,18 +1,37 @@
 //snake functions from header
 #include "stdafx.h"
 
+//update the physics world
 void update() {
 
 }
 
+//display position of target and player
 void display() {
 
 }
 
-void applyForces() {
-
+//apply force to snake
+void applyForces(b2Body& snake, char strInput) {
+	switch (strInput) {
+		case 'w':
+			snake.ApplyForceToCenter(b2Vec2(0,10),true);
+			break;
+		case 'a':
+			snake.ApplyForceToCenter(b2Vec2(-10, 0), true);
+			break;
+		case 's':
+			snake.ApplyForceToCenter(b2Vec2(0, -10), true);
+			break;
+		case 'd':
+			snake.ApplyForceToCenter(b2Vec2(10, 0), true);
+			break;
+		default:
+			break;
+	}
 }
 
+//move the target
 void moveTarget(float& xPos, float& yPos) {
 
 }
--- GravitySnake/GravitySnake/snake.h
+++ GravitySnake/GravitySnake/snake.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <Box2D\Box2D.h>
 
 void update();
 
 void display();
 
-void applyForces();
+void applyForces(b2Body& snake, char strInput);
 
 void moveTarget(float& xPos, float& yPos);
 
